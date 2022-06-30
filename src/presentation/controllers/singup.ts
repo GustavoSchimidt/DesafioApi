baaf580457d676a0683b89fe5edd9d82ae4bbb05
Gustavo Schimidt <schmidtgustavo7@gmail.com>
2022-06-30 09:34:01 -0400

    refactor: simplify code

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -4,7 +4,9 @@ import { httpRequest, httpResponse } from '../protocols/http'
 
 export class SingUpController {
   handle (httpRequest: httpRequest): httpResponse {
-    if (!httpRequest.body.name) return badRequest(new MissingParamsError('name'))
-    if (!httpRequest.body.email) return badRequest(new MissingParamsError('email'))
+    const requiredFields = ['name', 'email']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) return badRequest(new MissingParamsError(field))
+    }
   }
 }
