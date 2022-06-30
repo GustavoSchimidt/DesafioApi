2f4afd04aca68e1bd387a86ae53a7daa8979910e
Gustavo Schimidt <schmidtgustavo7@gmail.com>
2022-06-30 09:49:16 -0400

    refactor: add SingUpController in a protocol

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -1,9 +1,10 @@
 import { MissingParamsError } from '../errors/missingParamsError'
 import { badRequest } from '../helpers/http-helper'
-import { httpRequest, httpResponse } from '../protocols/http'
+import { Controller } from '../protocols/controller'
+import { HttpRequest, HttpResponse } from '../protocols/http'
 
-export class SingUpController {
-  handle (httpRequest: httpRequest): httpResponse {
+export class SingUpController implements Controller {
+  handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) return badRequest(new MissingParamsError(field))
--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,6 +1,6 @@
-import { httpResponse } from '../protocols/http'
+import { HttpResponse } from '../protocols/http'
 
-export const badRequest = (error: Error): httpResponse => ({
+export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
 })
