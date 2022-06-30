9d0e1a4b08c40da38945d4d03fd56a4934412db4
Gustavo Schimidt <schmidtgustavo7@gmail.com>
2022-06-30 13:34:08 -0400

    feat: ensure siginupController returns 200 if valid data is provided

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -25,11 +25,16 @@ export class SingUpController implements Controller {
       if (!emailIsValid) {
         return badRequest(new InvalidParamError('email'))
       }
-      this.addAccount.add({
+      const account = this.addAccount.add({
         name,
         email,
         password
       })
+
+      return {
+        statusCode: 200,
+        body: account
+      }
     } catch (error) {
       return serverError()
     }
